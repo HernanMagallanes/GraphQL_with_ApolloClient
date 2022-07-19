ae8e4c73692602c2c68c619ee0d9187b3d57e0c6
Hernán M <hernan.magallanes.94@gmail.com>
2022-07-19 06:54:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { gql, useQuery } from "@apollo/client";
+import { Persons } from "./persons";
+
+const ALL_PERSONS = gql`
+	query {
+		allPersons {
+			id
+			name
+			phone
+			address {
+				street
+				city
+			}
+		}
+	}
+`;
+
+function App() {
+	const { data, error, loading } = useQuery(ALL_PERSONS);
+
+	if (error) return <span style="color:red">{error}</span>;
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+
+				<h1>GraphQL + React !</h1>
+
+				{loading ? (
+					<p>Loading ...</p>
+				) : (
+					<Persons persons={data?.allPersons}></Persons>
+				)}
+			</header>
+		</div>
+	);
+}
+
+export default App;
